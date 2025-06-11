e6f53fcf732c21c3d592cae3b49885a9ffbf9f56
MrMani24 <manibaranriz8@gmail.com>
2025-06-11 13:15:34 +0330

    Commit I

---
+++ src/app/+navigations/public-navigation/ui/public-navigation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-public-navigation',
+  imports: [RouterOutlet],
+  templateUrl: './public-navigation.component.html',
+  styleUrl: './public-navigation.component.scss'
+})
+export class PublicNavigationComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { PublicNavigationComponent } from './+navigations/public-navigation/ui/public-navigation.component';
+import { HomeComponent } from './+pages/home/ui/home.component';
+import { ModelsComponent } from './+pages/models/ui/models.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'public',
+    component: PublicNavigationComponent,
+    children: [
+      { path: 'Home', component: HomeComponent },
+      { path: 'Models', component: ModelsComponent },
+      { path: '', redirectTo: 'Home', pathMatch: 'prefix' },
+    ],
+  },
+  { path: '', redirectTo: 'public', pathMatch: 'full' },
+];
